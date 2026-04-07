d84928f69ba26bf1c9086b989a306b79ba275794
Andres Hung <2018118240@ub.edu.bz>
2026-04-07 15:51:16 -0600

    feat: Complete lab 6A

--- a-counter-v2/app.js
+++ a-counter-v2/app.js
@@ -1,5 +1,5 @@
-import { emitter } from "./event-emitter.js";
-import { DataService } from "./data-service.js";
+import { emitter } from './event-emitter.js';
+import { DataService } from './data-service.js';
 
 const state = {
   count: 0,
@@ -8,28 +8,44 @@ const state = {
 function renderList() {
   // target the log-list class
   // be sure to check for errors
-  // Your code here
+  const log = document.querySelector('.log-list');
+  if (!log) {
+    return;
+  }
+
+  // construct timestamp string
+  const timestamp = new Date().toLocaleString([], {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
 
   // This code add a new entry to the log card
   // use the <li> tag
   if (state.count >= 1) {
-    log.innerHTML += ____________________________;
+    log.innerHTML += `<li>[${timestamp}] Count value updated to ${state.count}</li>`;
   }
 }
 
 function renderProgress() {
   // target the progress-fill class
   // be sure to check for errors
-  // Your code here
+  const progress = document.querySelector('.progress-fill');
+  if (!progress) {
+    return;
+  }
 
-  // This code chages the progress bar. Nothing to do here
+  // This code changes the progress bar. Nothing to do here
   progress.style.width = `${state.count}%`;
 }
 
 // Nothing to do here
 // All I did was change the name from render() to renderDisplay()
 function renderDisplay() {
-  const badge = document.querySelector(".badge");
+  const badge = document.querySelector('.badge');
   if (!badge) {
     return;
   }
@@ -40,19 +56,20 @@ function renderDisplay() {
 // component's render function is called
 function render() {
   renderList();
-  //  _______________
-  //  _______________
+  renderProgress();
+  renderDisplay();
 }
 
-emitter.on("count:changed", (count) => {
+emitter.on('count:changed', (count) => {
   state.count = count;
   render();
 });
 
-const button = document.querySelector("#btn-increment");
+const button = document.querySelector('#btn-increment');
 if (button) {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     DataService.increment();
   });
 }
+
 render();
--- a-counter-v2/data-service.js
+++ a-counter-v2/data-service.js
@@ -2,11 +2,8 @@ import { emitter } from './event-emitter.js';
 
 let count = 0;
 export const DataService = {
-    increment() {
-        count++;
-        emitter.emit('count:changed', count);
-    }
-}
-
-
-
+  increment() {
+    count++;
+    emitter.emit('count:changed', count);
+  },
+};
